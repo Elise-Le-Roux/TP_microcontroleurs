4563018e76e4eaeb36223dccc497391dc535553f
Elise Le Roux <lerouxde@etud.insa-toulouse.fr>
2021-09-19 22:46:02 +0200

    drivers

--- MesDrivers/Include/Driver_GPIO.h
+++ MesDrivers/Include/Driver_GPIO.h
@@ -9,14 +9,14 @@ typedef struct
 	char GPIO_Conf ; // voir ci dessous
 } MyGPIO_Struct_TypeDef ;
 
-#define In_Floating 0x0100
-#define In_PullDown 0x1000
-#define In_PullUp 0x1000
-#define In_Analog 0x0000
-#define Out_Ppull 0x0010
-#define Out_OD 0x0110
-#define AltOut_Ppull 0x1010
-#define AltOut_OD 0x1110
+#define In_Floating 0x4 //0b0100
+#define In_PullDown 0x8 //0b1000
+#define In_PullUp 0x8 //0b1000
+#define In_Analog 0x0 //0b0000
+#define Out_Ppull 0x2 //0b0010
+#define Out_OD 0x6 //0b0110
+#define AltOut_Ppull 0xA //0b1010
+#define AltOut_OD 0xE //0b1110
 
 void MyGPIO_Init ( MyGPIO_Struct_TypeDef * GPIOStructPtr ) ;
 int MyGPIO_Read ( GPIO_TypeDef * GPIO , char GPIO_Pin ) ; // renvoie 0 ou autre chose different de 0
