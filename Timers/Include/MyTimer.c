b60601c63de4142b5ae642fb1f2e7e43cf78abbc
Elise Le Roux <lerouxde@etud.insa-toulouse.fr>
2021-09-24 18:18:05 +0200

    interruptions

--- Timers/Include/MyTimer.c
+++ Timers/Include/MyTimer.c
@@ -1,17 +1,17 @@
 #include "MyTimer.h"
 
-void (* IT_function ) ( void ) ; /* Pointeur de fonction */
+void (* PtrF ) ( void ) ;  /* déclaration d’un pointeur de fonction */
 
 
 void MyTimer_Base_Init ( MyTimer_Struct_TypeDef * Timer ) {
-	
-	Timer->Timer->PSC = Timer->PSC; // Réglage de la période du Timer
-	Timer->Timer->ARR = Timer->ARR; 
 	
 	if (Timer->Timer == TIM1) RCC->APB2ENR |= RCC_APB2ENR_TIM1EN ; // Active l'horloge locale du périphérique
 	if (Timer->Timer == TIM2) RCC->APB1ENR |= RCC_APB1ENR_TIM2EN ;
 	if (Timer->Timer == TIM3) RCC->APB1ENR |= RCC_APB1ENR_TIM3EN ;
 	if (Timer->Timer == TIM4) RCC->APB1ENR |= RCC_APB1ENR_TIM4EN ;
+	
+	Timer->Timer->PSC = Timer->PSC; // Réglage de la période du Timer
+	Timer->Timer->ARR = Timer->ARR; 
 	
 	Timer->Timer->CR1 |=  (1 << 0); // Active le compteur
 }
@@ -19,6 +19,8 @@ void MyTimer_Base_Init ( MyTimer_Struct_TypeDef * Timer ) {
 
 void MyTimer_ActiveIT ( TIM_TypeDef * Timer , char Prio , void (* IT_function ) ( void ) ) {
 	char num_IT;
+	
+	PtrF = IT_function; /* affectation du pointeur */
 	
 	if (Timer == TIM1) num_IT = 25;        // Sélectionne le numéro d'interruption en fonction du timer
 	else if (Timer == TIM2) num_IT = 28;
@@ -28,6 +30,7 @@ void MyTimer_ActiveIT ( TIM_TypeDef * Timer , char Prio , void (* IT_function ) 
 	Timer->DIER |= (1 << 0); // Valide l'envoi d'une demande d'interruption
 	NVIC->IP[num_IT] |= (Prio << 4); // Fixe la priorité de l'interruption dans le NVIC
 	NVIC->ISER[0] |= (1 << num_IT); // Autorise la prise en compte de l'interruption dans le NVIC
+	
 }
 
 
@@ -39,22 +42,31 @@ void MyTimer_ActiveIT ( TIM_TypeDef * Timer , char Prio , void (* IT_function ) 
 void TIM1_UP_IRQHandler ( void )
 {
 	TIM1->SR &= ~(1 << 0); // Remet à 0 le flag d'interruption
+	if (PtrF != 0)
+		(*PtrF) (); /* appel indirect de la fonction */
 }
 
 
 void TIM2_IRQHandler ( void )
 {
-	TIM2->SR &= ~(1 << 0);
+	//TIM2->SR &= ~(1 << 0);
+	TIM2->SR &= ~TIM_SR_UIF;
+	if (PtrF != 0)
+		(*PtrF) (); /* appel indirect de la fonction */
 }
 
 
 void TIM3_IRQHandler ( void )
 {
 	TIM3->SR &= ~(1 << 0);
+	if (PtrF != 0)
+		(*PtrF) (); /* appel indirect de la fonction */
 }
 
 
 void TIM4_IRQHandler ( void )
 {
 	TIM4->SR &= ~(1 << 0);
+	if (PtrF != 0)
+		(*PtrF) (); /* appel indirect de la fonction */
 }
--- Timers/Source/principal.c
+++ Timers/Source/principal.c
@@ -2,6 +2,15 @@
 #include "MyTimer.h"
 #include "stm32f10x.h"
 
+MyGPIO_Struct_TypeDef GPIO_Struct;
+
+void CallBack ( void )
+{
+	/* Le code à exécuter pendant l’interruption */
+	MyGPIO_Toggle (GPIO_Struct.GPIO, GPIO_Struct.GPIO_Pin) ;
+}
+
+
 int main ( void ) {
 	
 //	TIM2->PSC = 0x1770; // 6000 
@@ -9,16 +18,25 @@ int main ( void ) {
 //	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN; // Clock enable
 //	TIM2->CR1 |=  (1 << 0); // Counter enable
 	
-	
+	// Configuration du timer
 	MyTimer_Struct_TypeDef TIM;
-	TIM.Timer = TIM2;
-	TIM.ARR = 0x7D0; // 2000
-	TIM.PSC = 0x4650; // 18k
+	TIM.Timer = TIM1;
+	TIM.ARR = 2000; 
+	TIM.PSC = 18000; 
+	
+	//TIM2->CR1 |= TIM_CR1_URS;
+	
+	// Configuration de la diode PA.5
+	GPIO_Struct.GPIO = GPIOA;
+	GPIO_Struct.GPIO_Pin = 5;
+	GPIO_Struct.GPIO_Conf = Out_Ppull;
+	MyGPIO_Init(&GPIO_Struct);
 	
 	MyTimer_Base_Init(&TIM);
-	//MyTimer_Base_Stop(TIM.Timer);
 	
-	MyTimer_ActiveIT(TIM2, 1);
+	MyTimer_ActiveIT(TIM1, 1, CallBack);
+	
+	
 do
 {
 }while (1) ;
